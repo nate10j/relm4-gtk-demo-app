2e3ac5b6e2a8f85e61516aad94845dd1de9c659b
Nathan Jang <nathanjang12@gmail.com>
2025-02-17 21:06:57 +0800

    reimplement with component macro

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use gtk::glib::clone;
-use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt};
+use gtk::prelude::{BoxExt, ButtonExt, OrientableExt, GtkWindowExt};
 use relm4::{gtk, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent};
 
 struct AppModel {
     counter: u8,
-    error: bool,
+    error: bool
 }
 
 #[derive(Debug)]
@@ -13,73 +12,56 @@ enum AppMsg {
     Decrement,
 }
 
-struct AppWidgets {
-    label: gtk::Label,
-}
-
+#[relm4::component]
 impl SimpleComponent for AppModel {
-
-    /// The type of the messages that this component can receive.
     type Input = AppMsg;
-    /// The type of the messages that this component can send.
     type Output = ();
-    /// The type of data with which this component will be initialized.
-    type Init = u8;
-    /// The root GTK widget that this component will create.
-    type Root = gtk::Window;
-    /// A data structure that contains the widgets that you will need to update.
-    type Widgets = AppWidgets;
+    type Init = ();
 
-    fn init_root() -> Self::Root {
-        gtk::Window::builder()
-            .title("Relm 4 Demo App")
-            .default_width(300)
-            .default_height(100)
-            .build()
+    view! {
+        gtk::Window {
+            set_title: Some("Relm 4 demo app"),
+            set_default_width: 300,
+            set_default_height: 100,
+
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_spacing: 5,
+                set_margin_all: 5,
+                gtk::Button {
+                    set_label: "+",
+                    connect_clicked => AppMsg::Increment
+                },
+
+                gtk::Button {
+                    set_label: "-",
+                    connect_clicked => AppMsg::Decrement
+                },
+                if model.error {
+                    gtk::Label {
+                        set_label: "error: exceeds limit of u8 variable",
+                        set_margin_all: 5,
+                    }
+                } else {
+                    gtk::Label {
+                        #[watch]
+                        set_label: &format!("Count: {}", model.counter),
+                        set_margin_all: 5,
+                    }
+                }
+            }
+        }
     }
 
     /// Initialize the UI and model.
     fn init(
-        counter: Self::Init,
-        window: Self::Root,
+        _params: Self::Init,
+        root: Self::Root,
         sender: ComponentSender<Self>,
-    ) -> relm4::ComponentParts<Self> {
-        let model = AppModel { counter, error: false };
+    ) -> ComponentParts<Self> {
+        let model = AppModel { counter: 0, error: false };
 
-        let vbox = gtk::Box::builder()
-            .orientation(gtk::Orientation::Vertical)
-            .spacing(5)
-            .build();
-
-        let inc_button = gtk::Button::with_label("Increment");
-        let dec_button = gtk::Button::with_label("Decrement");
-
-        let label = gtk::Label::new(Some(&format!("Counter: {}", model.counter)));
-        label.set_margin_all(5);
-
-        window.set_child(Some(&vbox));
-        vbox.set_margin_all(5);
-        vbox.append(&inc_button);
-        vbox.append(&dec_button);
-        vbox.append(&label);
-
-        inc_button.connect_clicked(clone!(
-            #[strong]
-            sender,
-            move |_| {
-                sender.input(AppMsg::Increment);
-            }
-        ));
-
-        dec_button.connect_clicked(clone!(
-            #[strong]
-            sender,
-            move |_| {
-                sender.input(AppMsg::Decrement);
-            }
-        ));
-
-        let widgets = AppWidgets { label };
+        let widgets = view_output!();
 
         ComponentParts { model, widgets }
     }
@@ -87,37 +69,26 @@ impl SimpleComponent for AppModel {
     fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {
         match message {
             AppMsg::Increment => {
-                if self.counter == u8::MAX {
+                if self.counter == 255 {
                     self.error = true;
-                    return;
+                    return
                 }
-                self.error = false;
                 self.counter += 1;
+                self.error = false;
             }
             AppMsg::Decrement => {
-                if self.counter == u8::MIN {
+                if self.counter == 0 {
                     self.error = true;
-                    return;
+                    return
                 }
-                self.error = false;
                 self.counter -= 1;
+                self.error = false;
             }
-        }
-    }
-
-    /// Update the view to represent the updated model.
-    fn update_view(&self, widgets: &mut Self::Widgets, _sender: ComponentSender<Self>) {
-        widgets
-            .label
-            .set_label(&format!("Counter: {}", self.counter));
-
-        if self.error {
-            widgets.label.set_label("error cannot go out of bounds of uint 8");
         }
     }
 }
 
 fn main() {
     let app = RelmApp::new("relm4.test.simple_manual");
-    app.run::<AppModel>(0);
+    app.run::<AppModel>(());
 }
